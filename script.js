f15fba0bf7f30510bfdf451935cba8ec1797757d
sakshi-builds <sharmasakshi1117@gmail.com>
2026-03-03 16:30:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function convertToRoman(num) {
-  let value=[1000,500,100,50,10,5,1];
+  let value=[1000,900,500,400,100,90,50,40,9,10,5,4,1];
 	let result="";
-	let symbol=['M','D','C','L','X','V','I'];
+	let symbol=['M',"CM",'D','CD','C','XC','L','XL','X','IX','V','IV','I'];
 	for(let i=0;i<value.length;i++){
 		while(num>=value[i]){
 			result=result+symbol[i];
